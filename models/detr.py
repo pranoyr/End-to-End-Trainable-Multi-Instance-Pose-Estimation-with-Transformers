58fef9426d14859e31afefec279619297d7513f3
Pranoy R <pranoy@firstsense.tech>
2021-12-22 12:38:43 +0530

    initial commit

--- models/detr.py
+++ models/detr.py
@@ -188,9 +188,8 @@ class SetCriterion(nn.Module):
 
 		Vgt_ = torch.repeat_interleave(V_gt , 2, dim=1)
 		offset_loss =  F.l1_loss(Vgt_  * Z_pred, Vgt_ * Z_gt, reduction = 'none')
-		V_gt = V_gt.type(torch.float32)
-		viz_loss  =  nn.MSELoss(V_pred, V_gt, reduction = 'none')
-		center_loss =  nn.MSELoss(C_pred , C_gt, reduction='none')
+		viz_loss  =  F.mse_loss(V_pred, V_gt, reduction = 'none')
+		center_loss =  F.mse_loss(C_pred , C_gt, reduction='none')
 		abs_loss = F.l1_loss(Vgt_ * A_pred, Vgt_ * A_gt, reduction='none')
 
 
