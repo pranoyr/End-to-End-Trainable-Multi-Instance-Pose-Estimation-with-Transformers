8f2181a5cb3816c82f1488fe9b0b72d129951f8e
Pranoy R <pranoy@firstsense.tech>
2021-12-22 10:36:24 +0530

    initial commit

--- main.py
+++ main.py
@@ -201,7 +201,8 @@ def main(args):
             checkpoint_paths = [output_dir / 'checkpoint.pth']
             # extra checkpoint before LR drop and every 100 epochs
             if (epoch + 1) % args.lr_drop == 0 or (epoch + 1) % 100 == 0:
-                checkpoint_paths.append(output_dir / f'checkpoint{epoch:04}.pth')
+                # checkpoint_paths.append(output_dir / f'checkpoint{epoch:04}.pth')
+                checkpoint_paths.append(output_dir / "model.pth")
             for checkpoint_path in checkpoint_paths:
                 utils.save_on_master({
                     'model': model_without_ddp.state_dict(),
