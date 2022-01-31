def39bc51a3c7a12c557188667a4d15c00b0b3a5
Pranoy R <pranoy@firstsense.tech>
2022-01-31 11:57:05 +0530

    init

--- engine.py
+++ engine.py
@@ -29,7 +29,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
         samples = samples.to(device)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
-        print(len(targets))
+        print(targets)
         outputs = model(samples)
         loss_dict = criterion(outputs, targets)
         weight_dict = criterion.weight_dict
