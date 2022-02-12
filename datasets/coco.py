2b7e37dd924bdcc17a46d78047204bc64fa6393c
Pranoy R <pranoyr@Pranoy-2.local>
2022-02-12 16:00:46 +0530

    max_size = 1333)

--- datasets/coco.py
+++ datasets/coco.py
@@ -209,8 +209,8 @@ def make_coco_transforms(image_set):
         T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
-    # scales = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
-    scales = [480, 512]
+    scales = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
+    # scales = [480, 512]
 
     # if image_set == 'train':
     #     return T.Compose([
@@ -226,24 +226,6 @@ def make_coco_transforms(image_set):
     #         normalize,
     #     ])
 
-
-
-    # if image_set == 'train':
-    #     return T.Compose([
-    #         T.RandomHorizontalFlip(),
-    #         T.RandomSelect(
-    #             T.Compose([
-    #             T.Rotate(0.5, [-25, 25]),
-    #             T.RandomResize(scales, max_size=1333),
-    #             ]),
-    #             T.Compose([
-    #                 T.RandomResize([400, 500, 600]),
-    #                 T.RandomSizeCrop(384, 600),
-    #                 T.RandomResize(scales, max_size=1333),
-    #             ])
-    #         ),
-    #         normalize,
-    #     ])
 
 
     if image_set == 'train':
@@ -252,16 +234,34 @@ def make_coco_transforms(image_set):
             T.RandomSelect(
                 T.Compose([
                 T.Rotate(0.5, [-25, 25]),
-                T.RandomResize(scales, max_size=512),
+                T.RandomResize(scales, max_size=1333),
                 ]),
                 T.Compose([
-                    T.RandomResize([400, 500]),
+                    T.RandomResize([400, 500, 600]),
                     T.RandomSizeCrop(384, 600),
-                    T.RandomResize(scales, max_size=512),
+                    T.RandomResize(scales, max_size=1333),
                 ])
             ),
             normalize,
         ])
+
+
+    # if image_set == 'train':
+    #     return T.Compose([
+    #         T.RandomHorizontalFlip(),
+    #         T.RandomSelect(
+    #             T.Compose([
+    #             T.Rotate(0.5, [-25, 25]),
+    #             T.RandomResize(scales, max_size=512),
+    #             ]),
+    #             T.Compose([
+    #                 T.RandomResize([400, 500]),
+    #                 T.RandomSizeCrop(384, 600),
+    #                 T.RandomResize(scales, max_size=512),
+    #             ])
+    #         ),
+    #         normalize,
+    #     ])
 
 
     if image_set == 'val':
